e2f08364bbc331a6fda8e8d42c8883552758c6eb
Юрий Уденеев <yudeneev@yandex.ru>
2020-02-14 09:51:13 +0400

    for branch testing

--- GeniyIdiotConsoleApp/GeniyIdiotConsoleApp/Program.cs
+++ GeniyIdiotConsoleApp/GeniyIdiotConsoleApp/Program.cs
@@ -1,12 +1,12 @@
 ﻿using System;
 
 namespace GeniyIdiotConsoleApp
-{
+{   // test
     class Program
     {
         static string[] GetQuestions(int countQuestions)
         {
-            // добавил комментарий длдя теста
+            // добавил комментарий для теста
             string[] questions = new string[countQuestions];
             questions[0] = "Сколько будет два плюс два  умноженное на два?";
             questions[1] = "Бревно нужно распилить на 10  частей, сколько надо сделать  распилов?";
