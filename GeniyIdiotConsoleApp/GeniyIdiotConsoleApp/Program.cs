67108f054729ff91f6127becb68e5cad0fbc4ebe
Юрий Уденеев <yudeneev@yandex.ru>
2020-02-18 18:40:29 +0400

    Был залит старый кад, исправил.

--- GeniyIdiotConsoleApp/GeniyIdiotConsoleApp/Program.cs
+++ GeniyIdiotConsoleApp/GeniyIdiotConsoleApp/Program.cs
@@ -1,69 +1,133 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace GeniyIdiotConsoleApp
-{   
+namespace GenyiIdiotConsoleApp
+{
     class Program
     {
-        static string[] GetQuestions(int countQuestions)
+        static void Main(string[] args)
         {
-            // test
-            // добавил комментарий для теста
-            string[] questions = new string[countQuestions];
-            questions[0] = "Сколько будет два плюс два  умноженное на два?";
-            questions[1] = "Бревно нужно распилить на 10  частей, сколько надо сделать  распилов?";
-            questions[2] = "На двух руках 10 пальцев. Сколько пальцев на 5 руках?";
-            questions[3] = "Укол делают каждые полчаса,  сколько нужно минут для трех  уколов?";
-            questions[4] = "Пять свечей горело, две  потухли. Сколько свечей  осталось?";
+            List<string> questions = GetQuestions();
 
-            return questions;
-        }
-        //Удалил неиспользуемые string [] args
-        static void Main()
-        {
-            int countQuestions = 5;
-            string[] questions = GetQuestions(countQuestions);
+            List<int> answers = GetAnswers();
 
-            int[] answers = new int[countQuestions];
-            answers[0] = 6;
-            answers[1] = 9;
-            answers[2] = 25;
-            answers[3] = 60;
-            answers[4] = 2;
+            int countQuestions = questions.Count;
 
-            int countRightAnswer = 0;
+            int countRightAnswers = 0;
+
+            Console.WriteLine("Здравствуйте! Представтесь, пожалуйста:");
+
+            string name = NameValidation(Console.ReadLine());
+
+            Console.WriteLine("\nОчень приятно " + name + ". Предлагаю сыграть со мной в игру." +
+                              "\nВаша задача ответить на несколько несложных вопросов, а после я выведу диагноз, насколько вы... кхм... дееспособны." +
+                              "\nИтак, как только будете готовы, нажмите любую клавишу, чтобы продолжить.");
+
+            Console.ReadKey(true);
 
             for (int i = 0; i < countQuestions; i++)
             {
-                Console.WriteLine("Вопрос №" + (i + 1));
-                int randomQuestionIndex = GetRandomQuestionIndex(countQuestions);
+                Console.WriteLine("\nВопрос " + (i + 1));
+
+                int randomQuestionIndex = GetRandomQuestionIndex(questions.Count);
+
                 Console.WriteLine(questions[randomQuestionIndex]);
 
-                int userAnswer = Convert.ToInt32(Console.ReadLine());
+                int userAnswer = ValidationCheck(Console.ReadLine());
 
                 int rightAnswer = answers[randomQuestionIndex];
 
-                if (userAnswer == rightAnswer)
-                {
-                    countRightAnswer++;
-                }
+                if (userAnswer == rightAnswer) countRightAnswers++;
+
+                DeleteAskedQestion(questions, answers, randomQuestionIndex);
+
             }
-            //test
-            string[] diagnoses = new string[6];
-            diagnoses[0] = "Идиот";
-            diagnoses[1] = "Кретин";
-            diagnoses[2] = "Дурак";
-            diagnoses[3] = "Нормальный";
-            diagnoses[4] = "Талант";
-            diagnoses[5] = "Гений";
-            Console.WriteLine("Ваш диагноз:" + diagnoses[countRightAnswer]);
+
+            Console.WriteLine("\nА вот и результаты, " + name + ":  ваш диагноз: " + GetDiagnose(countRightAnswers));
+
 
             Console.ReadKey();
         }
 
-        static int GetRandomQuestionIndex(int countQuestions)
+
+        static List<string> GetQuestions()
+        {
+            List<string> questions = new List<string>();
+
+            questions.Add("Сколько будет два плюс два  умноженное на два?");
+            questions.Add("Бревно нужно распилить на 10  частей, сколько надо сделать  распилов?");
+            questions.Add("На двух руках 10 пальцев. Сколько пальцев на 5 руках?");
+            questions.Add("Укол делают каждые полчаса, сколько нужно минут для трех уколов?");
+            questions.Add("Пять свечей горело, две  потухли. Сколько свечей  осталось?");
+
+            return questions;
+        }
+
+        static List<int> GetAnswers()
+        {
+            List<int> answers = new List<int>();
+
+            answers.Add(6);
+            answers.Add(9);
+            answers.Add(25);
+            answers.Add(60);
+            answers.Add(2);
+
+            return answers;
+        }
+
+        static string NameValidation(string name)
+        {
+            while (true)
+            {
+                bool isSpace = string.IsNullOrWhiteSpace(name);
+
+                if (!isSpace) return name;
+
+                Console.WriteLine("Вы ввели пустую строку. Попробуйте ещё раз:");
+
+                name = Console.ReadLine();
+            }
+        }
+
+        static int GetRandomQuestionIndex(int x)
         {
             Random random = new Random();
-            return random.Next(0, countQuestions);
+            return random.Next(x);
+        }
+
+        static int ValidationCheck(string userAnswer)
+        {
+            while (true)
+            {
+                bool isNum = int.TryParse(userAnswer, out int num);
+
+                if (isNum) return num;
+
+                Console.WriteLine("Некоректный формат ответа, попробуйте ещё раз:");
+
+                userAnswer = Console.ReadLine();
+            }
+        }
+
+        static void DeleteAskedQestion(List<string> questions, List<int> answers, int randomQuestionIndex)
+        {
+            answers.RemoveAt(randomQuestionIndex);
+            questions.RemoveAt(randomQuestionIndex);
+        }
+
+        static string GetDiagnose(int countRightAnswers)
+        {
+            string[] diagnoses = new string[6];
+
+            diagnoses[0] = "Идиот.";
+            diagnoses[1] = "Кретин.";
+            diagnoses[2] = "Дурак.";
+            diagnoses[3] = "Нормальный.";
+            diagnoses[4] = "Талант.";
+            diagnoses[5] = "Гений.";
+
+            return diagnoses[countRightAnswers];
         }
     }
 }
